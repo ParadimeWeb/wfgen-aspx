84ba93e071b1c67756daa5d78c79d84c60e56c7c
Gabriel Beauchamp <gabriel_beauchamp@paradimeweb.com>
2025-05-03 22:21:41 -0700

    SSO the right way

---
+++ WebServer/Modules/Auth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using Advantys.Security.Http;
+
+namespace ParadimeWeb.WorkflowGen.WebServer.Modules
+{
+    public class Auth : IHttpModule
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(HttpApplication application)
+        {
+            application.AuthenticateRequest += new EventHandler(AuthenticateRequest);
+        }
+
+        public void AuthenticateRequest(object source, EventArgs eventArgs)
+        {
+            var webApplication = (HttpApplication)source;
+            var context = webApplication.Context;
+
+            if (context.Request.Headers["Authorization"] == $"Bearer {ConfigurationManager.AppSettings["HooksBearerToken"]}")
+            {
+                context.Request.Headers.Add("IsWebhook", "Y");
+                return;
+            }
+
+            if (context.Request.Cookies["SSO"]?.Value == "1")
+            {
+                var wfgModule = new JWTAuthenticationModule();
+                wfgModule.AuthenticateRequest(source, eventArgs);
+            }
+        }
+    }
+}
