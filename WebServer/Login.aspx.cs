8ba2f6aa282f42abcea59c418f96af4dda362f95
Gabriel Beauchamp <gabriel_beauchamp@paradimeweb.com>
2025-05-06 12:03:31 -0700

    Multi auth

---
+++ WebServer/Login.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using Advantys.Security;
+using Jose;
+using System.Web;
+
+namespace ParadimeWeb.WorkflowGen.WebServer
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private static readonly string SessionTokenCookie = !string.IsNullOrEmpty(ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenCookie"]) ? ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenCookie"] : "wfgen_token";
+        private static readonly byte[] SessionTokenSigningSecret = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenSigningSecret"]);
+
+        protected System.Web.UI.WebControls.TextBox txtUsername;
+        protected System.Web.UI.WebControls.TextBox txtPassword;
+        protected System.Web.UI.WebControls.Label errorLabel;
+
+        private void redirect()
+        {
+            var returnUrl = Request["ReturnUrl"] == null ? string.Empty : Request["ReturnUrl"];
+            Response.Redirect(returnUrl.EndsWith("show.aspx?QUERY=CONTEXT&REQUEST_QUERY=WELCOME&NO_REDIR=Y") ? "~/" : returnUrl);
+        }
+
+        protected void btnSSO_Click(object Source, EventArgs e)
+        {
+            Response.Cookies.Add(new HttpCookie("wfgen_auth_type", "SSO") { HttpOnly = true, SameSite = SameSiteMode.Lax });
+            redirect();
+        }
+        protected void btnSubmit_Click(object Source, EventArgs e)
+        {
+            var username = txtUsername.Text;
+            var password = txtPassword.Text;
+
+            var connectionString = ConfigurationManager.ConnectionStrings["MainDbSource"].ConnectionString;
+            var providerName = ConfigurationManager.ConnectionStrings["MainDbSource"].ProviderName;
+            var removeDomainPrefix = ConfigurationManager.AppSettings["ApplicationSecurityRemoveDomainPrefix"].Split(',').GetEnumerator();
+            int maxLoginAttempts;
+            if (!int.TryParse(ConfigurationManager.AppSettings["ApplicationSecurityMaxLoginAttempts"], out maxLoginAttempts))
+                maxLoginAttempts = 5;
+            var applicationSecurityPasswordManagementMode = "V5";
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["ApplicationSecurityPasswordManagementMode"]))
+                applicationSecurityPasswordManagementMode = ConfigurationManager.AppSettings["ApplicationSecurityPasswordManagementMode"];
+            try
+            {
+                new UserIdentity(providerName, connectionString, removeDomainPrefix).Authenticate(username, password, maxLoginAttempts, applicationSecurityPasswordManagementMode);
+            }
+            catch
+            {
+                errorLabel.Text = "Invalid crendentials";
+                return;
+            }
+
+            var token = JWT.Encode(username, SessionTokenSigningSecret, JwsAlgorithm.HS256);
+            Response.Cookies.Add(new HttpCookie(SessionTokenCookie, token) { HttpOnly = true, SameSite = SameSiteMode.Lax });
+            Response.Cookies.Add(new HttpCookie("wfgen_auth_type", "WFGEN") { HttpOnly = true, SameSite = SameSiteMode.Lax });
+
+            redirect();
+        }
+    }
+}
--- WebServer/Modules/Auth.cs
+++ WebServer/Modules/Auth.cs
@@ -1,53 +1,65 @@
 ﻿using System;
 using System.Configuration;
+using System.Security.Principal;
+using System.Text;
 using System.Web;
 using Advantys.Security.Http;
+using Jose;
 
 namespace ParadimeWeb.WorkflowGen.WebServer.Modules
 {
     public class Auth : IHttpModule
     {
-        public void Dispose()
-        {
-            
-        }
+        private static readonly string SessionTokenCookie = !string.IsNullOrEmpty(ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenCookie"]) ? ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenCookie"] : "wfgen_token";
+        private static readonly byte[] SessionTokenSigningSecret = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["ApplicationSecurityAuthSessionTokenSigningSecret"]);
 
+        public void Dispose() {}
         public void Init(HttpApplication application)
         {
-            application.BeginRequest += Context_BeginRequest;
-            application.AuthenticateRequest += new EventHandler(AuthenticateRequest);
+            var context = application.Context;
+            application.BeginRequest += new EventHandler(beginRequest);
+            application.AuthenticateRequest += new EventHandler(authenticateRequest);
         }
 
-        public void AuthenticateRequest(object source, EventArgs eventArgs)
+        private void authenticateRequest(object source, EventArgs e)
         {
             var webApplication = (HttpApplication)source;
-            var context = webApplication.Context;
-
-            if (context.Request.Headers["Authorization"] == $"Bearer {ConfigurationManager.AppSettings["HooksBearerToken"]}")
+            if (webApplication.Request.Headers["Authorization"] == $"Bearer {ConfigurationManager.AppSettings["HooksBearerToken"]}")
             {
-                context.Request.Headers.Add("IsWebhook", "Y");
+                webApplication.Request.Headers.Add("IsWebhook", "Y");
                 return;
             }
-
-            if (context.Request.Cookies["SSO"]?.Value == "1")
+            if (webApplication.Request.Url.AbsoluteUri.Contains("/wfgen/login.aspx"))
             {
-                var wfgModule = new JWTAuthenticationModule();
-                wfgModule.AuthenticateRequest(source, eventArgs);
+                return;
             }
-        }
-
-        private void Context_BeginRequest(object sender, EventArgs e)
+            if (webApplication.Request.Cookies["wfgen_auth_type"]?.Value == "SSO")
+            {
+                var jwtModule = new JWTAuthenticationModule();
+                jwtModule.AuthenticateRequest(source, e);
+                return;
+            }
+            var sessionToken = webApplication.Request.Cookies[SessionTokenCookie]?.Value;
+            if (string.IsNullOrEmpty(sessionToken))
+            {
+                webApplication.Response.Redirect($"~/login.aspx?ReturnUrl={HttpUtility.UrlEncode(webApplication.Request.Url.PathAndQuery)}");
+                return;
+            }
+            var decodedToken = JWT.Decode(sessionToken, SessionTokenSigningSecret, JwsAlgorithm.HS256);
+            webApplication.Context.User = new GenericPrincipal(new GenericIdentity(decodedToken), null);
+        } 
+        
+        private void beginRequest(object source, EventArgs e)
         {
-            var application = (HttpApplication)sender;
-            var context = application.Context;
+            var application = (HttpApplication)source;
             var delegatorCookiename = "WFGEN_ID_USER_DELEGATOR";
-            var ID_USER_DELEGATOR = context.Request.Form["ID_USER_DELEGATOR"] ?? "-1";
-            var delegatorCookie = context.Request.Cookies[delegatorCookiename];
+            var ID_USER_DELEGATOR = application.Request.Form["ID_USER_DELEGATOR"] ?? "-1";
+            var delegatorCookie = application.Request.Cookies[delegatorCookiename];
             if (delegatorCookie == null || delegatorCookie.Value != ID_USER_DELEGATOR)
             {
                 if (delegatorCookie == null)
                 {
-                    context.Response.Cookies.Add(new HttpCookie(delegatorCookiename, ID_USER_DELEGATOR)
+                    application.Response.Cookies.Add(new HttpCookie(delegatorCookiename, ID_USER_DELEGATOR)
                     {
                         HttpOnly = true,
                         SameSite = SameSiteMode.Lax
@@ -58,7 +70,7 @@ namespace ParadimeWeb.WorkflowGen.WebServer.Modules
                     delegatorCookie.Value = ID_USER_DELEGATOR;
                     delegatorCookie.HttpOnly = true;
                     delegatorCookie.SameSite = SameSiteMode.Lax;
-                    context.Response.Cookies.Set(delegatorCookie);
+                    application.Response.Cookies.Set(delegatorCookie);
                 }
             }
         }
